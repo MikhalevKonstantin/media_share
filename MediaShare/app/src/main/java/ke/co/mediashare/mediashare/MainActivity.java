b1cfaaef4ef0516750525d8101e9407d41263da7
xts400 <programmer.billionaire@gmail.com>
2015-06-02 03:51:17 +0300

    Add code to set Typefaces for the Start Screen TextViews

--- MediaShare/app/src/main/java/ke/co/mediashare/mediashare/MainActivity.java
+++ MediaShare/app/src/main/java/ke/co/mediashare/mediashare/MainActivity.java
@@ -2,20 +2,52 @@ package ke.co.mediashare.mediashare;
 
 import android.app.ActionBar;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.app.Activity;
 import android.view.View;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
+    private TextView hello_text;
+    private TextView welcome_text;
+    private TextView tag_line_text;
+    private TextView slide_text;
+    private TextView media_share_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Setting Typeface for the Start Screen's TextViews
+        hello_text = (TextView)findViewById(R.id.hello_text);
+        Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/RobotoCondensedBold.ttf");
+        hello_text.setTypeface(typeface);
+
+        welcome_text = (TextView)findViewById(R.id.welcome_text);
+        Typeface typeface1 = Typeface.createFromAsset(getAssets(), "fonts/RobotoCondensedRegular.ttf");
+        welcome_text.setTypeface(typeface1);
+
+        media_share_text = (TextView)findViewById(R.id.media_share_text);
+        Typeface typeface4 = Typeface.createFromAsset(getAssets(), "fonts/RobotoCondensedBold.ttf");
+        media_share_text.setTypeface(typeface4);
+
+
+        tag_line_text = (TextView)findViewById(R.id.tag_line_text);
+        Typeface typeface2 = Typeface.createFromAsset(getAssets(), "fonts/RobotoCondensedRegular.ttf");
+        tag_line_text.setTypeface(typeface2);
+
+        slide_text = (TextView)findViewById(R.id.slide_text);
+        Typeface typeface3 = Typeface.createFromAsset(getAssets(), "fonts/RobotoCondensedLight.ttf");
+        slide_text.setTypeface(typeface3);
+
+
+
 
     }
 
