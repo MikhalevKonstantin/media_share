ad9c94d5b7f2bbed74691a4e103afd4226771688
xts400 <programmer.billionaire@gmail.com>
2015-06-03 11:53:51 +0300

    Refactor class to extend Fragment

--- MediaShare/app/src/main/java/ke/co/mediashare/mediashare/NewReleasesActivity.java
+++ MediaShare/app/src/main/java/ke/co/mediashare/mediashare/NewReleasesActivity.java
@@ -1,38 +1,28 @@
 package ke.co.mediashare.mediashare;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 
 
-public class NewReleasesActivity extends ActionBarActivity {
+public class NewReleasesActivity extends Fragment {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_new_releases);
-    }
 
+    }
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_new_releases, menu);
-        return true;
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        View view = inflater.inflate(R.layout.activity_new_releases, container, false);
+
+        return view;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
