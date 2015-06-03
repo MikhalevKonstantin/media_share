e46b72ccfa8372e1055a1873090bac64262d17ab
xts400 <programmer.billionaire@gmail.com>
2015-06-03 11:51:03 +0300

    Refactor class to extend Fragment

--- MediaShare/app/src/main/java/ke/co/mediashare/mediashare/DiscoverBooksActivity.java
+++ MediaShare/app/src/main/java/ke/co/mediashare/mediashare/DiscoverBooksActivity.java
@@ -2,6 +2,7 @@ package ke.co.mediashare.mediashare;
 
 import java.util.Locale;
 
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
@@ -10,6 +11,7 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -19,170 +21,52 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 
-public class DiscoverBooksActivity extends ActionBarActivity implements ActionBar.TabListener {
-
-    /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-     */
-    SectionsPagerAdapter mSectionsPagerAdapter;
-
-    /**
-     * The {@link ViewPager} that will host the section contents.
-     */
-    ViewPager mViewPager;
+public class DiscoverBooksActivity extends ActionBarActivity{
+    private static final String TAG ="media_share";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_discover_books);
+        setTabsUp(savedInstanceState);
 
-        // Set up the action bar.
-        final ActionBar actionBar = getSupportActionBar();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState){
+
+        // Save the index of the selected tab so that it is reselected when the orientation changes
+        outState.putInt("tabIndex", getSupportActionBar().getSelectedNavigationIndex());
+    }
+
+    private void setTabsUp(Bundle savedInstanceState){
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.action_bar_color)));
         actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        ActionBar.Tab best_sellers_tab = actionBar.newTab();
+        ActionBar.Tab new_releases_tab = actionBar.newTab();
+        ActionBar.Tab recommendations_tab = actionBar.newTab();
 
-        // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.pager);
-        mViewPager.setAdapter(mSectionsPagerAdapter);
+        BestSellersActivity bestSellersActivity = new BestSellersActivity();
+        best_sellers_tab.setText("BEST SELLERS").setContentDescription("best_sellers_tab").setTabListener(new MediaShareTabListener<BestSellersActivity>(bestSellersActivity));
 
-        // When swiping between different sections, select the corresponding
-        // tab. We can also use ActionBar.Tab#select() to do this if we have
-        // a reference to the Tab.
-        mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
-            @Override
-            public void onPageSelected(int position) {
-                actionBar.setSelectedNavigationItem(position);
-            }
-        });
+        NewReleasesActivity newReleasesActivity = new NewReleasesActivity();
+        new_releases_tab.setText("NEW RELEASES").setContentDescription("new_releases_tab").setTabListener(new MediaShareTabListener<NewReleasesActivity>(newReleasesActivity));
 
-        // For each of the sections in the app, add a tab to the action bar.
-        for (int i = 0; i < mSectionsPagerAdapter.getCount(); i++) {
-            // Create a tab with text corresponding to the page title defined by
-            // the adapter. Also specify this Activity object, which implements
-            // the TabListener interface, as the callback (listener) for when
-            // this tab is selected.
-            actionBar.addTab(
-                    actionBar.newTab()
-                            .setText(mSectionsPagerAdapter.getPageTitle(i))
-                            .setTabListener(this));
+        RecommendedBooksActivity recommendedBooksActivity = new RecommendedBooksActivity();
+        recommendations_tab.setText("RECOMMENDATIONS").setContentDescription("recommended_books_tab").setTabListener(new MediaShareTabListener<RecommendedBooksActivity>(recommendedBooksActivity));
+
+        actionBar.addTab(best_sellers_tab);
+        actionBar.addTab(new_releases_tab);
+        actionBar.addTab(recommendations_tab);
+
+        if (savedInstanceState != null) {
+            Log.i(TAG, "setting selected tab from saved bundle");
+//            get the saved selected tab's index and set that tab as selected
+            actionBar.setSelectedNavigationItem(savedInstanceState.getInt("tabIndex", 0));
         }
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_discover_books, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
-        // When the given tab is selected, switch to the corresponding page in
-        // the ViewPager.
-        mViewPager.setCurrentItem(tab.getPosition());
-    }
-
-    @Override
-    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
-    }
-
-    @Override
-    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
-    }
-
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
-        }
-
-        @Override
-        public int getCount() {
-            // Show 3 total pages.
-            return 3;
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            Locale l = Locale.getDefault();
-            switch (position) {
-                case 0:
-                    return getString(R.string.title_section1).toUpperCase(l);
-                case 1:
-                    return getString(R.string.title_section2).toUpperCase(l);
-                case 2:
-                    return getString(R.string.title_section3).toUpperCase(l);
-            }
-            return null;
-        }
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_discover_books, container, false);
-            return rootView;
-        }
-    }
 
 }
