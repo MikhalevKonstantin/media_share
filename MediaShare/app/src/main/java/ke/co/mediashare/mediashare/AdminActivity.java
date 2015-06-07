baf5ec86073a324d9a8d7174ea4f8cfecbf904e6
xts400 <programmer.billionaire@gmail.com>
2015-06-07 14:48:45 +0300

    Add Activity for Admin Services

---
+++ MediaShare/app/src/main/java/ke/co/mediashare/mediashare/AdminActivity.java
@@ -0,0 +1,60 @@
+package ke.co.mediashare.mediashare;
+
+import android.content.DialogInterface;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.app.AlertDialog;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class AdminActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.action_bar_color)));
+    }
+
+    public void bookCategoryChooser(View view){
+        AlertDialog.Builder booksChooserDialog = new AlertDialog.Builder(this);
+        booksChooserDialog.setTitle("Book Category");
+        booksChooserDialog.setItems(R.array.books_categories, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        AlertDialog alertDialog = booksChooserDialog.create();
+        alertDialog.show();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_admin, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
