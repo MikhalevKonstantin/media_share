df565287fa1a96a012bc9c65499b729157b3b5d5
xts400 <programmer.billionaire@gmail.com>
2015-06-07 14:54:45 +0300

    Add click listener for the Admin button

--- MediaShare/app/src/main/java/ke/co/mediashare/mediashare/DashboardActivity.java
+++ MediaShare/app/src/main/java/ke/co/mediashare/mediashare/DashboardActivity.java
@@ -14,6 +14,7 @@ import android.widget.Button;
 public class DashboardActivity extends ActionBarActivity {
     private Button btn_discover_books;
     private Button btn_my_profile;
+    private Button admin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +24,7 @@ public class DashboardActivity extends ActionBarActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.action_bar_color)));
 
+        admin = (Button)findViewById(R.id.btn_admin);
         btn_my_profile = (Button)findViewById(R.id.btn_my_profile);
         btn_discover_books = (Button)findViewById(R.id.btn_discover_books);
         btn_discover_books.setOnClickListener(new View.OnClickListener() {
@@ -38,6 +40,15 @@ public class DashboardActivity extends ActionBarActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), ProfileActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Setting onClick listener for Admin
+        admin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent().setClass(getApplicationContext(), AdminActivity.class);
                 startActivity(intent);
             }
         });
