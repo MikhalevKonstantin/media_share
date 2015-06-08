aa42718182029b55bebafad219cb0153dd481bb4
xts400 <programmer.billionaire@gmail.com>
2015-06-08 19:19:22 +0300

    Set Activity to use ToolBar layout

--- MediaShare/app/src/main/java/ke/co/mediashare/mediashare/ProfileActivity.java
+++ MediaShare/app/src/main/java/ke/co/mediashare/mediashare/ProfileActivity.java
@@ -1,18 +1,10 @@
 package ke.co.mediashare.mediashare;
 
-import android.graphics.drawable.ColorDrawable;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
+import android.support.v7.widget.Toolbar;
 import android.widget.ListView;
 
 
@@ -22,6 +14,7 @@ public class ProfileActivity extends ActionBarActivity {
     private ActionBarDrawerToggle actionBarDrawerToggle;
     private String[] drawer_list_items;
     private Integer[] drawer_image_list;
+    private Toolbar toolbar;
 
 
     @Override
@@ -29,9 +22,9 @@ public class ProfileActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.setHomeAsUpIndicator(R.drawable.ic_action_home);
-        actionBar.setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.action_bar_color)));
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        toolbar.setNavigationIcon(R.drawable.ic_action_home);
+        setSupportActionBar(toolbar);
 
         // Set Navigation List image drawables
         drawer_image_list = new Integer[]{R.drawable.ic_profile, R.drawable.ic_order_history, R.drawable.ic_wish_list, R.drawable.ic_action_io};
@@ -45,7 +38,7 @@ public class ProfileActivity extends ActionBarActivity {
         drawerLayout = (DrawerLayout) findViewById(R.id.DrawerLayout);
 
         // Set the ActionBarDrawerToggle
-        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.my_profile, R.string.my_profile);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,R.string.my_profile, R.string.my_profile);
 
         // Set the ArrayAdapter for the Navigation ListView
         MediaShareListAdapter adapter = new MediaShareListAdapter(this, drawer_image_list, drawer_list_items);
