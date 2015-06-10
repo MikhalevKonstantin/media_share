dd83941c938cda5b5aef0094ac1a27ae8d7013ec
xts400 <programmer.billionaire@gmail.com>
2015-06-10 04:29:44 +0300

    Refactor Activity to implement RecyclerView instead of ListView

--- MediaShare/app/src/main/java/ke/co/mediashare/mediashare/ProfileActivity.java
+++ MediaShare/app/src/main/java/ke/co/mediashare/mediashare/ProfileActivity.java
@@ -4,17 +4,24 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.widget.ListView;
+import android.view.View;
 
 
 public class ProfileActivity extends ActionBarActivity {
-    private ListView navigation_list_view;
     private DrawerLayout drawerLayout;
     private ActionBarDrawerToggle actionBarDrawerToggle;
     private String[] drawer_list_items;
-    private Integer[] drawer_image_list;
+    private int[] drawer_image_list;
     private Toolbar toolbar;
+    private String profile_name = "Dennis Mwebia";
+    private String profile_email = "dennis@mediashare.co.ke";
+    private int profile_photo = R.drawable.read_later;
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter recylerAdapter;
+    private RecyclerView.LayoutManager recyclerLayoutManager;
 
 
     @Override
@@ -22,30 +29,42 @@ public class ProfileActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-        toolbar = (Toolbar)findViewById(R.id.toolbar);
-        toolbar.setNavigationIcon(R.drawable.ic_action_home);
+        // Defining Toolbar
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         // Set Navigation List image drawables
-        drawer_image_list = new Integer[]{R.drawable.ic_profile, R.drawable.ic_order_history, R.drawable.ic_wish_list, R.drawable.ic_action_io};
+        drawer_image_list = new int[]{R.drawable.ic_home, R.drawable.ic_profile, R.drawable.ic_order_history, R.drawable.ic_wish_list, R.drawable.ic_action_io};
         // Set the Navigation List Items
         drawer_list_items = getResources().getStringArray(R.array.navigation_drawer_array);
 
-        // Set the Navigation ListView
-        navigation_list_view = (ListView) findViewById(R.id.NavigationListView);
+        // Set the Navigation RecycleView
+        recyclerView = (RecyclerView) findViewById(R.id.navigation_recycler_view);
+        recyclerView.setHasFixedSize(true);
+        recylerAdapter = new MediaShareListAdapter(drawer_list_items, drawer_image_list, profile_name, profile_email, profile_photo);
+        recyclerView.setAdapter(recylerAdapter);
+        recyclerLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(recyclerLayoutManager);
 
         // Set the DrawerLayout
         drawerLayout = (DrawerLayout) findViewById(R.id.DrawerLayout);
 
         // Set the ActionBarDrawerToggle
-        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,R.string.my_profile, R.string.my_profile);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.my_profile, R.string.my_profile) {
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                toolbar.setTitle("Home");
+            }
 
-        // Set the ArrayAdapter for the Navigation ListView
-        MediaShareListAdapter adapter = new MediaShareListAdapter(this, drawer_image_list, drawer_list_items);
-        navigation_list_view.setAdapter(adapter);
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                toolbar.setTitle("My Account");
+            }
+        };
+
         drawerLayout.setDrawerListener(actionBarDrawerToggle);
-
-
+        actionBarDrawerToggle.syncState();
     }
+
 
 }
