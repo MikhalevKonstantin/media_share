bf5e9c341fb0f09bbc468422e248df1e5ffa7563
Dennis Mwebia <programmer.billionaire@gmail.com>
2015-07-27 18:00:34 +0300

    Add new database model/table for storing libraries

---
+++ MediaShare/app/src/main/java/ke/co/mediashare/mediashare/ke/co/mediashare/mediashare/database/Libraries.java
@@ -0,0 +1,10 @@
+package ke.co.mediashare.mediashare.ke.co.mediashare.mediashare.database;
+
+/**
+ * Created by guidovanrossum on 27/07/15.
+ */
+public class Libraries {
+	public static final String TABLE_NAME="table_libraries";
+	public static final String LIBRARY_ID="_id";
+	public static final String LIBRARY_NAME="library_name";
+}
